326dc08d51363517672ff16712e485eee51b5695
MarcStelzner <stelzner@informatik.uni-luebeck.de>
2013-03-01 15:50:22 +0100

    cleaning up old files

--- main/Main.java
+++
@@ -1,8 +0,0 @@
-package packettracking.main;
-public class Main {
-	public static void main(String[] args) {
-		Constructor coordinator = new Constructor(); 
-		boolean testRun = true;
-		coordinator.run(testRun);
-	}
-}
